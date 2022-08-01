a4d65772d65f4a9c5869576b4d69a23f5f16a558
Osazee Odigie <osazee.odigie01@gmail.com>
2022-08-01 18:26:44 -0400

    commiting a few changes

--- create_inventory_file.sh
+++
@@ -1,2 +0,0 @@
-echo '[all]' > inventory
-echo "aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filters 'Name=tag:Project','Values=udacity' --output text" >> inventory
